d9bb5b3d0c17d6d1c9384e3ed3c8c5de6e406129
sre404 <serioandresp660@gmail.com>
2026-03-21 17:52:54 -0400

    feat: add GitHub Actions release workflow

--- AudioVisualizer/build.gradle.kts
+++ AudioVisualizer/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 
     defaultConfig {
         minSdk = 24
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
@@ -41,8 +42,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     // Required so maven-publish can locate the release component
